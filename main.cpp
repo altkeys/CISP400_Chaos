6e5948a05a85fd2eaf3e99d917f72f52354eee27
altkeys <155630430+altkeys@users.noreply.github.com>
2024-11-13 18:42:53 -0800

    Random colors, thread sleeps

--- main.cpp
+++ main.cpp
@@ -1,63 +1,83 @@
-/*
- *    CISP 400 Chaos Game !!!
- */
-
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 #include <iostream>
 #include <sstream>
-#include <vector>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <thread>
+#include <chrono>
 
-const int POINTS_PER_FRAME = 10,
-          MAX_POINTS       = 50000;
-
-/* SFML stuff */
 using namespace sf;
-Color generate_random_color();
-void draw_points(RenderWindow& scene, std::vector<Vector2f> points);
+using namespace std;
 
+const int POINTS_PER_FRAME = 1000,
+          MAX_POINTS = 25000;
+
+Color generate_random_color();
+CircleShape create_point(Vector2f position, Color color);
 
 int main() {
     srand(time(0));
+    int loaded_points = 0;
 
-    VideoMode vm(1920, 1080);
-    RenderWindow window(vm, "Chaos Game !!!", Style::Default);
-    std::vector<Vector2f> vertices, points;
+    VideoMode screen(1920, 1080);
+    RenderWindow window(screen, "Chaos Game !!!!", Style::Default);
+    vector<CircleShape> vertices, points;
 
     while (window.isOpen()) {
         Event event;
-        while(window.pollEvent(event)) {
+        Color color = generate_random_color();
+
+        while (window.pollEvent(event)) {
             if (event.type == Event::Closed) { window.close(); }
 
             if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
-                std::cout << "(" << event.mouseButton.x << ", " << event.mouseButton.y
-                  << std::endl;
-
+                cout << "(" << event.mouseButton.x << ", " << event.mouseButton.y << ")"
+                  << endl;
+                
                 if (vertices.size() < 3) {
-                    vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-                } else if (points.size() == 0){
-                    points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    Vector2f position(event.mouseButton.x, event.mouseButton.y);
+                    vertices.push_back(create_point(position, color));
+                } else if (points.size() == 0) {
+                    Vector2f position(event.mouseButton.x, event.mouseButton.y);
+                    points.push_back(create_point(position, color));
                 }
             }
         }
 
         if (Keyboard::isKeyPressed(Keyboard::Escape)) { window.close(); }
-        if (points.size() > 0) {
+        if (points.size() > 0) { 
+
+            if (loaded_points == MAX_POINTS) {
+                vector<CircleShape> new_points(points.begin() + 1000, points.end());
+                points.swap(new_points);
+
+                loaded_points -= POINTS_PER_FRAME;
+            }
+
             for (int i = 0; i < POINTS_PER_FRAME; i++) {
                 int index = rand() % vertices.size();
 
-                double x_pos = ( (points.back().x + vertices.at(index).x) / 2.0 );
-                double y_pos = ( (points.back().y + vertices.at(index).y) / 2.0 );
+                CircleShape vertex = vertices.at(index),
+                            point = points.back();
 
-                points.push_back(Vector2f(x_pos, y_pos));
+                double x_pos = ( (point.getPosition().x + vertex.getPosition().x) / 2.0 ),
+                       y_pos = ( (point.getPosition().y + vertex.getPosition().y) / 2.0 );
+
+                Vector2f position(x_pos, y_pos);
+                points.push_back(create_point(position, color));
             }
+
+            loaded_points += POINTS_PER_FRAME;
+            this_thread::sleep_for(chrono::milliseconds(500));
         }
 
         window.clear();
-        draw_points(window, vertices);
-        draw_points(window, points);
+        for (CircleShape& vertex : vertices) { window.draw(vertex); }
+
+        for (CircleShape& point : points) {
+            window.draw(point);
+        }
         window.display();
     }
 
@@ -65,20 +85,18 @@ int main() {
 }
 
 Color generate_random_color() {
-
     int red   = rand() % 256,
         green = rand() % 256,
         blue  = rand() % 256;
 
-    /* There is an alpha channel for transparancy but nahhh */
     return Color(red, green, blue);
 }
 
-void draw_points(RenderWindow& scene, std::vector<Vector2f> points) {
-    for (size_t i = 0; i < points.size(); i++) {
-        CircleShape point(5);
-        point.setPosition(Vector2f(points.at(i).x, points.at(i).y));
-        point.setFillColor(generate_random_color());
-        scene.draw(point);
-    }
+CircleShape create_point(Vector2f position, Color color) {
+    CircleShape point(3);
+
+    point.setPosition(position);
+    point.setFillColor(color);
+
+    return point;
 }
